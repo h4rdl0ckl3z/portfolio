da90a5315f99c54bc24e961ee1e46d2dfc87224a
b4nkz-5pch <bank427ingza@gmail.com>
2025-03-06 08:50:38 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white text-center py-4">
             <div className="container mx-auto">
-                <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+                <p>&copy; { new Date().getFullYear() } My Portfolio. All rights reserved.</p>
                 {/* <div className="flex justify-center space-x-4 mt-4">
                     <a href="#" className="hover:text-gray-400"><i className="fab fa-facebook-f"></i></a>
                     <a href="#" className="hover:text-gray-400"><i className="fab fa-twitter"></i></a>
