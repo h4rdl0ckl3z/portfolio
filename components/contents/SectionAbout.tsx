d4d666d1e8a7cc2e4ee671e6b8dd12de88fc3842
h4rdl0ckl3z <bank427ingza@gmail.com>
2025-06-20 10:21:06 +0700

    update work&profile

--- components/contents/SectionAbout.tsx
+++ components/contents/SectionAbout.tsx
@@ -6,11 +6,10 @@ export const SectionAbout = () => {
             <div className="container mx-auto text-center">
                 <h2 className="text-3xl font-bold mb-4">About Me</h2>
                 <p className="text-xl mb-8">
-                    I am a qualified and skilled developer with learning and experience.
-                    I always code with an eye for detail.
-                    I am ready to learn new things and develop myself.
+                    I am very interested in applying for this position or any related positions within the company. I would like to utilize my knowledge and skills in contributing to the organization. I am highly motivated to join, learn and grow together with the company.
                 </p>
-                <Image src="https://avatars.githubusercontent.com/u/54482333?s=400&u=c430a54568014a47938386f3aa960d7cb97f9125&v=4" width={200} height={200} alt="A detailed portrait of the developer" className="mx-auto rounded-full" />
+                {/* <Image src="https://avatars.githubusercontent.com/u/54482333?s=400&u=c430a54568014a47938386f3aa960d7cb97f9125&v=4" width={200} height={200} alt="A detailed portrait of the developer" className="mx-auto rounded-full" /> */}
+                <Image src="https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/463428306_2443436085999428_8517229882710112264_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHGKEU_74jKHZvFgypWrvTVbvw_I_jz7cNu_D8j-PPtw2wpNZ1CHsArxNiFP3anCX83ZNyQmiLwrOsz5rYVcK4W&_nc_ohc=Gsbfk6DC2CQQ7kNvwE73X3U&_nc_oc=AdnzZDSisoR1l6PwmsUUeuXrlHEKkSB3ZmvxsBNcsbVw9-LxI0159JqqfG5pn2cOiyoxGlYK9hwGTpy6KW7fw4gY&_nc_zt=23&_nc_ht=scontent.fbkk8-4.fna&_nc_gid=M0WvXBgTt14rSXRKPeKxGg&oh=00_AfPTIroIAgknFrDb2QNNY6S1TRFDamB5Wr3cfJ4jLi_X5A&oe=685A948C" width={200} height={200} alt="A detailed portrait of the developer" className="mx-auto rounded-full" />
             </div>
         </section>
     );
--- components/contents/SectionWork.tsx
+++ components/contents/SectionWork.tsx
@@ -1,28 +1,36 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames';
+
+type EventItem = {
+    year: string;
+    title: string;
+    description: string;
+    details: string[];
+};
 
 export const SectionWork = () => {
     const { theme } = useTheme();
     const [isClient, setIsClient] = useState(false);
+    const [events, setEvents] = useState<EventItem[]>([]);
 
     useEffect(() => {
         setIsClient(true);
+
+        const fetchData = async () => {
+            const res = await import('@/data/work-events.json');
+            setEvents(res.default);
+        };
+
+        fetchData();
     }, []);
 
     const border = theme === 'light' ? 'border-black' : 'border-white';
     const bg = theme === 'light' ? 'bg-black' : 'bg-white';
 
-    const events = [
-        { year: 'Full Stack Developer | 11/12/2024 - 5/3/2025', title: 'Elegance Consultant Co.,Ltd.', description: 'Full Stack Developer with Devops and CI/CD', details: ['SingleSignOn (KeyCloak Provider)', 'SSO Client Realm Management (NextJS)', 'Authentication Feat. SSO (C# .NET)'] },
-        { year: 'Developer | PartTime 2023', title: 'STS System & Development Co.,Ltd.', description: '', details: ['Development Auto e-GP (Thai Government Procurement)'] },
-        { year: 'Student Project | 2023', title: 'Development of A Management System For Teaching Materials', description: 'Management durable articles of Science and Technology faculty, add the durable articles to the system, lending and return, fixing, sale and tracking', details: ['Design the database (MySQL)', 'Coding in PHP, JavaScript, J-Query, AJAX', 'Manual Testing'] },
-        { year: 'Application Developer | 2022', title: 'STS System & Development Co.,Ltd.', description: '', details: ['Programming the Auto e-GP (Thai Government Procurement)', 'Convert Database (Access to MariaDB)', 'IT-Support (Network and Server, Install and Update OS, Hardware)', 'Customer-Support', 'Assistance (Developer, Design UX/UI for the website, Web service-Planning)'] }
-    ];
-
-    const EventItem = ({ year, title, description, details }: { year: string, title: string, description: string, details: string[] }) => (
+    const TimelineItem = ({ year, title, description, details }: EventItem) => (
         <div className="flex items-center mb-8">
             <div className="w-1/2 text-right pr-8">
                 <p className="text-lg">{year}</p>
@@ -48,9 +56,9 @@ export const SectionWork = () => {
             <div className="relative py-4 pr-4">
                 <div className={classNames('absolute left-1/2 transform -translate-x-1/2 h-full border-l-2', isClient ? border : 'border-transparent')}></div>
                 {events.map((event, index) => (
-                    <EventItem key={index} year={event.year} title={event.title} description={event.description} details={event.details} />
+                    <TimelineItem key={index} {...event} />
                 ))}
             </div>
         </section>
     );
-}
+};
